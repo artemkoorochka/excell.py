4dad6347f661a720a4e1afefb6127c748ee508dc
artemkoorochka <artem@koorochka.com>
2021-06-16 19:55:13 +0300

    Json cli from .xls

--- related.py
+++ related.py
@@ -1,6 +1,9 @@
 import xlrd
+import sys
+
 # /home/bitrix/www/upload/koorochka/excel/
-file = 'related_import.xls'
+file = sys.argv[1]
+# file = "related_import.xlsx"
 
 workbook = xlrd.open_workbook(file)
 sheet_names = workbook.sheet_names()
@@ -8,13 +11,15 @@ worksheet = workbook.sheet_by_index(0)
 current = 0
 json = ''
 
-json += "{"
+json += "["
 for i in range(1,worksheet._dimnrows):
     if str(worksheet.cell(i,1).value) != '':
         if str(worksheet.cell(i,0).value) != '':
             current = worksheet.cell(i,0).value
-        json += "'{}':'{}'".format(current, worksheet.cell(i,1).value)
+        json += "{"
+        json += '"product":"{}", "related":"{}"'.format(current, worksheet.cell(i,1).value)
+        json += "}"
         if i < (worksheet._dimnrows - 1):
             json += ","
-json += "}"
+json += "]"
 print(json)
