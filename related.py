0701a1b4ce15aee3480cf419fb586a9d9ce89b19
artemkoorochka <artem@koorochka.com>
2021-07-12 17:57:00 +0300

    read file by cols

--- related.py
+++ related.py
@@ -3,19 +3,31 @@ import sys
 
 # /home/bitrix/www/upload/koorochka/excel/
 file = sys.argv[1]
-# file = "test17.xls"
+# file = "related_sample.xls"
+# file = "related_sample_4.xls"
+
+start = 1
+product = 0
+related = 2
+sort = 4
+count = 0
 
 workbook = xlrd.open_workbook(file)
 worksheet = workbook.sheet_by_index(0)
 json = ''
 
 json += "["
-for i in range(1,worksheet._dimnrows):
-    if str(worksheet.cell(i,1).value) != '':
-        json += "{"
-        json += '"product":"{}", "related":"{}", "sort":"{}"'.format(worksheet.cell(i,0).value, worksheet.cell(i,2).value, worksheet.cell(i,4).value)
-        json += "}"
-        if i < (worksheet._dimnrows - 1):
+
+for i in range(0, len(worksheet.col_values(product, start))):
+    if worksheet.col_values(product,start)[i] != '':
+        if i > count:
             json += ","
+        json += "{"
+        json += '"product":"{}", "related":"{}", "sort":"{}"'.format(worksheet.col_values(product,start)[i],
+                                                                     worksheet.col_values(related,start)[i],
+                                                                     worksheet.col_values(sort,start)[i])
+        json += "}"
+        count = count + 1
+
 json += "]"
 print(json)
